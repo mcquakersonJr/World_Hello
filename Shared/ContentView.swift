1793b681f6bdac4d7e109a30a9b7aec14d40a693
Ethan Lai <ethanylai@gmail.com>
2021-07-04 21:48:33 -0700

    hi

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Ethan Lai on 6/27/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Fart")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/World_HelloApp.swift
@@ -0,0 +1,17 @@
+//
+//  World_HelloApp.swift
+//  Shared
+//
+//  Created by Ethan Lai on 6/27/21.
+//
+
+import SwiftUI
+
+@main
+struct World_HelloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
