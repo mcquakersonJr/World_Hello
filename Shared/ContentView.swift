3a9ef9451a941a9a94990e3057d5a6a44d9acbb0
Ethan Lai <ethanylai@gmail.com>
2021-07-10 16:35:37 -0700

    First working version

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -6,13 +6,76 @@
 //
 
 import SwiftUI
+import AVFoundation
+
+
+
+struct splashView: View {
+
+    @State var isActive:Bool = false
+    
+    var body: some View {
+        VStack {
+            if self.isActive {
+                // go to main view
+                ContentView()
+            } else {
+                //Image("AwesomeSplash")
+// An alternative Text version of the splash screen
+                Text("Fart!")
+                    .font(Font.largeTitle)
+            }
+        }
+        .onAppear() {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
+                withAnimation {
+                    self.isActive = true
+                }
+            }
+        }
+    }
+    
+}
+
 
 struct ContentView: View {
     var body: some View {
-        Text("Fart")
-            .padding()
-    }
+        Button(action: {
+            playAudioAsset("gross")
+          }, label: {
+            Text("Fart")
+                .padding()
+    })
+        
+        }
 }
+
+
+var audioPlayer: AVAudioPlayer!
+
+// Here is a simple function to play a sound from Assets.xcassets
+func playAudioAsset(_ assetName : String) {
+   guard let audioData = NSDataAsset(name: assetName)?.data else {
+      fatalError("Unable to find asset \(assetName)")
+   }
+
+   do {
+      audioPlayer = try AVAudioPlayer(data: audioData)
+      audioPlayer.play()
+   } catch {
+      fatalError(error.localizedDescription)
+   }
+}
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
--- Shared/World_HelloApp.swift
+++ Shared/World_HelloApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct World_HelloApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            splashView()
         }
     }
 }
